4c5c463fb9379404fdc74bab8c497bc1f7b41661
黄洪伟 <huanghongwei@yzw.cn>
2020-11-18 11:10:15 +0800

    bbbb

--- EasyUI.WPF/CustomControl1.cs
+++ EasyUI.WPF/CustomControl1.cs
@@ -49,7 +49,7 @@ namespace EasyUI.WPF
         static CustomControl1()
         {
             DefaultStyleKeyProperty.OverrideMetadata(typeof(CustomControl1), new FrameworkPropertyMetadata(typeof(CustomControl1)));
-            var x = "1";
+            var x = "1"+"2";
         }
     }
 }
